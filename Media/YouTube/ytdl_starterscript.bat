43c749959eb8fb9cde72b402be091c44ebe592be
Andrew Brandt <brandt.andrew89@gmail.com>
2019-02-10 19:18:15 -0600

    Complete starter script

--- Media/YouTube/ytdl_starterscript.bat
+++ Media/YouTube/ytdl_starterscript.bat
@@ -5,12 +5,22 @@ echo Downloading from YouTube!
 set DRIVE_LETTER=Q
 
 ::REM set the path on the drive to download to
-set TARGET_PATH=YouTube
+set ROOT_PATH=YouTube
 
 ::REM build the full path
-set FP=%DRIVE_LETTER%:\%TARGET_PATH%\
+set FP=%DRIVE_LETTER%:\%ROOT_PATH%\
 
 ::REM set up non-parameter args
-set NPA=
+set NPA=-ciw -v --embed-thumbnail --write-all-thumbnails --embed-subs --all-subs --add-metadata
+
+::REM set up parameter args
+set FMT=-f bestvideo+bestaudio
+set OUTFMT=--merge-output-format mkv
+set OUT=-o "%FP%\<LOC>\%(channel)s\%(upload_date)s_%(title)s.%(ext)s"
+set INFILE=--batch-file <FILE_OF_URLS>
+set ARCHFILE=--download-archive <ARCHIVE_FILE>
+
+::REM Full Command
+youtube-dl %NPA% %FMT% %OUTFMT% %OUT% %ARCHFILE% %INFILE%
 
 pause
