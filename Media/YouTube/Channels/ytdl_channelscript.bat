3b66a67622338a5f679f2747dc8191d542ddf267
Andrew Brandt <brandt.andrew89@gmail.com>
2019-02-10 19:40:06 -0600

    Fix problem with channel downloader

--- Media/YouTube/Channels/ytdl_channelscript.bat
+++ Media/YouTube/Channels/ytdl_channelscript.bat
@@ -1,17 +1,16 @@
-title YouTube DL
-echo Downloading from YouTube!
-
 ::REM set up non-parameter args
-set NPA=-ciw -v --embed-thumbnail --write-all-thumbnails --embed-subs --all-subs --add-metadata 
+::set NPA=-ciw -v --embed-thumbnail --write-all-thumbnails --embed-subs --all-subs --add-metadata 
 
 ::REM set up parameter args
-set FMT=-f bestvideo+bestaudio 
-set OUTFMT=--merge-output-format mkv 
-set OUT=-o ".\Channels\%(channel)s\%(upload_date)s_%(title)s.%(ext)s" 
-set INFILE=--batch-file ".\ChannelURLs.txt" 
-set ARCHFILE=--download-archive ".\ChannelVidsCompltedDL.txt" 
+::set FMT=-f bestvideo+bestaudio 
+::set OUTFMT=--merge-output-format mkv 
+::set OUT=-o ".\Channels\%(channel)s\%(upload_date)s_%(title)s.%(ext)s" 
+::set INFILE=--batch-file ".\ChannelURLs.txt" 
+::set ARCHFILE=--download-archive ".\ChannelVidsCompltedDL.txt" 
 
 ::REM Full Command
-youtube-dl %NPA% %FMT% %OUTFMT% %OUT% %INFILE% %ARCHFILE%
+::youtube-dl %NPA% %FMT% %OUTFMT% %OUT% %INFILE% %ARCHFILE%
+
+youtube-dl -ciw -v --embed-thumbnail --write-all-thumbnails --embed-subs --all-subs --add-metadata -f bestvideo+bestaudio --merge-output-format mkv --download-archive ".\ChannelVidsCompltedDL.txt" -o ".\Channels\%%(channel)s\%%(upload_date)s_%%(title)s.%%(ext)s" --batch-file ".\ChannelURLs.txt"
 
 pause
