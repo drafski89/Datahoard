e67dfff9459c74d3cb3b918391470be04060620a
Andrew Brandt <brandt.andrew89@gmail.com>
2019-02-10 19:32:09 -0600

    Channel script works in powershell, not as a .bat script

--- Media/YouTube/Channels/ytdl_channelscript.bat
+++ Media/YouTube/Channels/ytdl_channelscript.bat
@@ -1,26 +1,17 @@
 title YouTube DL
 echo Downloading from YouTube!
 
-::REM set the drive letter of the target drive to download to
-set DRIVE_LETTER=Q
-
-::REM set the path on the drive to download to
-set ROOT_PATH=YouTube
-
-::REM build the full path
-set FP=%DRIVE_LETTER%:\%ROOT_PATH%\
-
 ::REM set up non-parameter args
-set NPA=-ciw -v --embed-thumbnail --write-all-thumbnails --embed-subs --all-subs --add-metadata
+set NPA=-ciw -v --embed-thumbnail --write-all-thumbnails --embed-subs --all-subs --add-metadata 
 
 ::REM set up parameter args
-set FMT=-f bestvideo+bestaudio
-set OUTFMT=--merge-output-format mkv
-set OUT=-o "%FP%\Channels\%(channel)s\%(upload_date)s_%(title)s.%(ext)s"
-set INFILE=--batch-file ".\ChannelURLs.txt"
-set ARCHFILE=--download-archive ".\ChannelCompletedDLs.txt"
+set FMT=-f bestvideo+bestaudio 
+set OUTFMT=--merge-output-format mkv 
+set OUT=-o ".\Channels\%(channel)s\%(upload_date)s_%(title)s.%(ext)s" 
+set INFILE=--batch-file ".\ChannelURLs.txt" 
+set ARCHFILE=--download-archive ".\ChannelVidsCompltedDL.txt" 
 
 ::REM Full Command
-youtube-dl %NPA% %FMT% %OUTFMT% %OUT% %ARCHFILE% %INFILE%
+youtube-dl %NPA% %FMT% %OUTFMT% %OUT% %INFILE% %ARCHFILE%
 
 pause
