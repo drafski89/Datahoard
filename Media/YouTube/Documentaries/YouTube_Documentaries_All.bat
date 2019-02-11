814a374b101f9058a65abd6bccf198d8c4c86e6f
Andrew Brandt <brandt.andrew89@gmail.com>
2019-02-10 18:57:12 -0600

    Add ytdl update all documentaries bat script

---
+++ Media/YouTube/Documentaries/YouTube_Documentaries_All.bat
@@ -0,0 +1,24 @@
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=CBtjRJXEzIQ
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=TPHlhzRSEnw
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=NuLQ4gqB5XE
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=K3VQULyT390
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=p-I8GDklsN4
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=W6Uuex4VZPE&feature=youtu.be
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=uXI3H6Kd3K4
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=Rw7P0RGZQxQ
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=Y888wVY5hzw&feature=youtu.be
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=GV5WMCmwHqc&feature=youtu.be
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=-fny99f8amM&feature=youtu.be
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=uXOPcMMHCJA&feature=youtu.be
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=6ZRgVX8SYX4
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=aYNwRogs5SY
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=pYa_3oh1-s4
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=zmN-tgPRYmk
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=y3qkf3bajd4&feature=youtu.be
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=nmJ5q3mH78A&index=40&list=WL
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=aVTOr7Nq2SM
+youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\Documentaries\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv https://www.youtube.com/watch?v=-esjQLvsgTs&t=29s
+::youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv 
+::youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv 
+::youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv 
+::youtube-dl -f bestvideo+bestaudio -ciw -o "Q:\Data_Andrew\Media\YouTube\%%(upload_date)s_%%(title)s.%%(ext)s" -v --merge-output-format mkv 
